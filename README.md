f8015785caa6aa57775f2c15a61bf688ed6db288
Adam Rosendorff <adam.rosendorff@gmail.com>
2018-01-02 13:38:31 +0200

    Rework of extension
    Rewriting of the extension to simplify it.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 tabstop
 =======
 
-A very simple Google Chrome extension to block tabs loading on startup that I made.
+A very simple Google Chrome extension to block tabs loading on startup.
 Feel free to take the code and do whatever you want with it.
 I'd welcome all suggestions and comments.
--- tabstop.js
+++ tabstop.js
@@ -16,66 +16,27 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var open = []; //list to store all open tabs at launch
-var loaded = false;
+var block_tabs = [];
 
 //runs when chrome starts
-chrome.runtime.onStartup.addListener(function(start){
-  chrome.tabs.query({active : true}, function(openTab){//prevents open tab blocking
-
-    //gets tab ID of active tab
-    var currentTab = openTab[0].id;
-    //getting tab IDs for all external http/s URLs
-    chrome.tabs.query({url : "http://*/*"},function(tabs){
-      for (var i = 0; i < tabs.length; i++) {
-        if (currentTab != tabs[i].id) {//checks it's not the active tab's ID
-          open.push(tabs[i].id);
-        }
+chrome.runtime.onStartup.addListener(function(start) {
+  chrome.tabs.query({}, function(tabs) { // find all open tabs
+    tabs.forEach(function(details) {
+      if (details.url) { // check if a tab has a url (not an internal tab)
+        block_tabs.push(details.id);
       }
-    });
-    chrome.tabs.query({url : "https://*/*"},function(tabs){
-      for (var i = 0; i < tabs.length; i++) {
-        if (currentTab != tabs[i].id) {//checks it's not the active tab's ID
-          open.push(tabs[i].id);
-        }
-      }
-      for (var i = 0; i < open.length; i++) {
-        chrome.tabs.reload(open[i]);//reloads all tabs to ensure they are correctly blocked
-      }
-      loaded = true;
     });
   });
 });
 
-function block(tab){
-
-  if (open.indexOf(tab.tabId) != -1 && loaded == true) {//checking if the tab has been reloaded
-    return {cancel: true};//this blocks the web request
+// blocks all web requests
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+  var tabIndex = block_tabs.indexOf(details.tabId);
+  if (tabIndex !== -1) { // if the requests came from a tab open at startup
+    block_tabs.splice(tabIndex, 1); // remove the tab from the list of tabs to be blocked
+    return {cancel: true}; // block the tab
   }
-}
-
-function remove(tab){
-  if (open.indexOf(tab.tabId) != -1) {//if the tab hasn't yet been reloaded
-    open[open.indexOf(tab.tabId)] = undefined;//remove tab ID from array
-    chrome.tabs.reload(tab.tabId);
-  }
-  var done = true;
-  for (var i = 0; i < open.length; i++) {
-    if (open[i] != undefined) {//check if all tabs are reloaded (empty array)
-      done = false;
-    }
-  }
-  if (done == true) {//when all tabs are reloaded, remove uneeded listeners
-    console.log("DONE");
-    chrome.tabs.onActivated.removeListener(remove);
-    chrome.webRequest.onBeforeRequest.removeListener(block);
-  }
-}
-
-//runs when the active tab is changed
-chrome.tabs.onActivated.addListener(remove);
-
-//blocks all requests from tabs
-chrome.webRequest.onBeforeRequest.addListener(block,
-  {urls: ["<all_urls>"]},
-  ["blocking"]);
+  return {cancel: false}
+},
+{urls: ['<all_urls>']},
+['blocking']);
